3d248820a2a10c5eddee6bf55ba82d00538308ac
= <=>
2022-11-02 14:00:17 -0300

    expo-av

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Image, Pressable, Text, View } from "react-native";
+import { Audio } from "expo-av";
 
 import { IData } from "../../services/types";
 
@@ -14,8 +15,28 @@ const Card: React.FC<IData> = ({
   artist,
   duration,
 }: IData) => {
+  const [song, setSong] = useState<any>();
+
+  const playSong = async () => {
+    const { sound: playbackObject } = await Audio.Sound.createAsync(
+      { uri: preview },
+      { shouldPlay: true }
+    );
+    setSong(playbackObject);
+    await playbackObject.unloadAsync();
+  };
+
+  useEffect(() => {
+    return song
+      ? () => {
+          console.log("Unloading Sound");
+          song.unloadAsync();
+        }
+      : undefined;
+  }, [song]);
+
   return (
-    <Pressable>
+    <Pressable onPress={() => playSong()}>
       <View style={styles.container}>
         <Image source={{ uri: album.cover_big }} style={styles.image} />
         <View style={styles.content}>
